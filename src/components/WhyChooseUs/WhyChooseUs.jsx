34d5adb7e722a4b7d181c0e5085b7949f2565908
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-10-16 16:02:04 +0100

    updated

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { FaEye, FaPeopleGroup } from "react-icons/fa6";
-import { GiGraduateCap } from "react-icons/gi";
 import { motion } from "framer-motion";
 import { SlideLeft } from "../../utility/animation";
-import { FaHospital, FaRocket } from "react-icons/fa";
-import { Link } from "react-router-dom"; // Assuming you are using React Router for navigation
+import { Link } from "react-router-dom";
 
 const WhyChooseData = [
   {
     id: 1,
     title: "VISION",
     desc: "To be a provider of social services that are easily available to use.",
-    icon: <FaRocket />,
+   icon: "https://www.meshnigeria.org/wp-content/uploads/2023/09/5481993.png",
     link: "/vision",
-    bgColor: "#0063ff",
     delay: 0.3,
   },
   {
@@ -21,8 +17,7 @@ const WhyChooseData = [
     title: "MISSION",
     desc: "To ensure the quality of life of humanity",
     link: "/mission",
-    icon: <FaEye />,
-    bgColor: "#73bc00",
+    icon: "https://www.meshnigeria.org/wp-content/uploads/2023/09/rocket-icon-startup-icon-free-vector.jpg",
     delay: 0.6,
   },
   {
@@ -30,8 +25,7 @@ const WhyChooseData = [
     title: "EDUCATION",
     desc: "MESH is poised to carry out activities that would improve education in areas that are in dire need.",
     link: "/education",
-    icon: <GiGraduateCap />,
-    bgColor: "#fa6400",
+    icon: "https://www.meshnigeria.org/wp-content/uploads/2023/09/graduation-hat-icon-simple-college-diploma-vector-39330116.jpg",
     delay: 0.9,
   },
   {
@@ -39,8 +33,7 @@ const WhyChooseData = [
     title: "SOCIAL",
     desc: "MESH in addition to Education and Health, provides social services to the less privileged in Nigeria.",
     link: "/social",
-    icon: <FaPeopleGroup />,
-    bgColor: "#fe6baa",
+    icon: "https://www.meshnigeria.org/wp-content/uploads/2023/09/images-2.png",
     delay: 1.2,
   },
   {
@@ -48,8 +41,7 @@ const WhyChooseData = [
     title: "HEALTH",
     desc: "As much as possible, reaching out with free medical care to disadvantaged areas and like Education and Social, cover a significant area.",
     link: "/health",
-    icon: <FaHospital />,
-    bgColor: "#45d345",
+    icon: "https://www.meshnigeria.org/wp-content/uploads/2023/09/istockphoto-1295833781-612x612-1.jpg",
     delay: 1.5,
   },
 ];
@@ -68,17 +60,15 @@ const WhyChooseUs = () => {
               variants={SlideLeft(item.delay)}
               initial="hidden"
               whileInView={"visible"}
-              className="space-y-4 p-6 rounded-xl shadow-[0_0_22px_rgba(0,0,0,0.15)]"
-            >
-              <div
-                style={{ backgroundColor: item.bgColor }}
-                className="w-10 h-10 rounded-lg flex justify-center items-center text-white"
-              >
-                <div className="text-2xl">{item.icon}</div>
+              className="space-y-4 p-6 rounded-xl shadow-[0_0_22px_rgba(0,0,0,0.15)]">
+              <div className="w-20 h-20 rounded-xl flex justify-center items-center bg-white">
+                <img src={item.icon} alt={item.title} />
               </div>
               <p className="font-semibold">{item.title}</p>
               <p className="text-sm text-gray-500 text-justify">{item.desc}</p>
-              <Link to={item.link} className="text-primary font-semibold hover:underline py-2">
+              <Link
+                to={item.link}
+                className="text-primary font-semibold underline hover:underline-offset-4 py-2">
                 Learn More
               </Link>
             </motion.div>
