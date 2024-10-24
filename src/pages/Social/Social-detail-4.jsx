774b51b8be5e05d16c8ab3e9770f6f4989ff7731
Yusuf Abdullahi Temidayo <yusufabdullahi2910@gmail.com>
2024-10-24 12:01:24 +0100

    updated

--- src/pages/Social/Social-detail-4.jsx
+++ src/pages/Social/Social-detail-4.jsx
@@ -1,11 +1,217 @@
-import React from 'react'
+import React from "react";
 
 const SocialDetails4 = () => {
   return (
-    <div>
-      
+    <div className="px-4 lg:px-8 mt-20 py-12 space-y-5">
+      <h1 className="text-center text-2xl lg:text-4xl font-bold text-secondary">
+        Life-skills Training for the Deaf Community in 4 states (Ilorin, Gombe,
+        Lafia and FCT)
+      </h1>
+      <p className="text-lg font-normal text-justify">
+        Life Skills for the Training of Deaf communities is a strategic approach
+        for empowering the deaf with knowledge that would keep them
+        self-employed. There were five (5) Life – Skills training for different
+        Deaf groups.
+      </p>
+      <p className="text-2xl text-secondary font-semibold">
+        Abuja – FCT Life – Skills Training
+      </p>
+      <img
+        className="mx-auto"
+        src="https://www.meshnigeria.org/wp-content/uploads/2022/02/01_Pictures-with-Deaf-Executives-478-by-179.jpg"
+        alt="MESH Nigeria"
+      />
+      <p className="text-lg text-secondary font-semibold text-center">
+        Two trainings were organized in Abuja, FCT.
+      </p>
+      <p className="text-lg font-normal text-justify">
+        The first training was organized on 5th of August 2017 for both men and
+        women deaf people while the second training was also held in Abuja on
+        22nd August 2017 and it was mainly for deaf women. Both trainings were
+        on the skills for surviving the challenges of life. The objective of the
+        trainings was to provide skills about life that could make living a lot
+        easier for people by especially working on mindset. At both training
+        events, there were video clips on causes of deafness, its symptoms,
+        deafness management and deaf daily activities. There were also
+        power-point presentations on sex education, marriage counseling and
+        parenting in deaf families. A total number of 192 deaf attended the
+        trainings.
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/03c_Group-Photo-with-Deaf-People-474-by-354-1-400x284.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Group Photo with Deaf People
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/04_Female-Participants-520-by-388-1-2-400x284.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Female Participants
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/01_Pictures-with-Deaf-Executives-478-by-179-400x284.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Pictures with Deaf Executives
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/02_Male-Session-298-by-398_Redited-2.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Male Session
+          </p>
+        </span>
+      </div>
+      <p className="text-2xl text-secondary font-semibold">
+        Gombe – Gombe State Life – Skills Training
+      </p>
+      <p className="text-lg font-normal text-justify">
+        On the 6th and 7th of December, 2017 MESH organized a similar training
+        at the ETF Education Resource Centre, Station Dukku in Gombe. There were
+        130 participants some dignitaries such as the National President for the
+        deaf in Nigeria, Mallam Ishiaku Adamu and the Gombe State chapter of
+        Joint Association of Person Living with Disability (JONAPWD) that was
+        represented by Hon Aligoro.
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/02_Participants-from-Gomber-484-by-340-2.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Participants from Gombe
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/03_Participants-from-Gomber444-by340-1.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Participants from Gombe
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/01_Participants-from-Gomber-1.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Participants from Gombe
+          </p>
+        </span>
+      </div>
+
+      <p className="text-2xl text-secondary font-semibold">
+        Ilorin – Kwara State Life – Skills Training
+      </p>
+      <p className="text-lg font-normal text-justify">
+        MESH held a 2-day training for the Muslim Deaf in Ilorin on 4th and 5th
+        of October 2017. A total of ninety (90) participants attended the
+        training. The objective of the training was to teach the deaf about
+        survival strategy and coping with life challenges.
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/03_Participans-from-Ilorin-400x284.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold">
+            Participants from Ilorin
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/01_Participans-from-Ilorin-400x284.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold">
+            Participants from Ilorin
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/02b_Participans-from-Ilorin.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold">
+            Participants from Ilorin
+          </p>
+        </span>
+      </div>
+
+      <p className="text-lg text-secondary font-semibold text-center">
+      Lafia – Nasarawa State Life – Skills Training
+      </p>
+      <p className="text-lg font-normal text-justify">
+        The last training was held in Lafia on 1st and 2nd February 2018. It was
+        also for the deaf community in Nasarawa State. There were 60 students
+        mobilized from the Secondary School for Special Needs Education and 46
+        non-students in the training. The Special Adviser to the Governor on
+        Gender and Disability Matters, Dr. (Hon) Hajarat Danyaro Ibrahim, gave a
+        goodwill message and formally opened the training. Soon after the
+        training, a proposal was prepared on behalf of the Nasarawa State Deaf
+        Association and submitted to the Nasarawa Government with the hope of
+        getting funds to organize more training in the State, offer vocational
+        training and increase access to healthcare information by engaging sign
+        interpreters at the government health facilities.
+      </p>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10">
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/Participants-from-Lafia-for-the-Life-Skills-Training188.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Participant from Lafia
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/Participants-from-Lafia-for-the-Life-Skills-Training567-by-378.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Trainer from Lafia
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/Trainer-from-Lafia.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Participant from Lafia
+          </p>
+        </span>
+        <span>
+          <img
+            src="https://www.meshnigeria.org/wp-content/uploads/2022/02/Participants-from-Lafia-for-the-Life-Skills-Training348.jpg"
+            alt="MESH Nigeria"
+          />
+          <p className="text-lg text-primary font-semibold text-center">
+            Participant from Lafia
+          </p>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SocialDetails4;
